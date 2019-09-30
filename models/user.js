aeb17296fa0d08a6040a849ec646cbe08f153027
Jisu Kim <vbalien@live.jp>
2019-10-01 02:30:48 +0900

    모델 코드 리펙토링

--- models/user.js
+++ models/user.js
@@ -1,28 +1,34 @@
-module.exports = (sequelize, DataTypes) => {
-    return sequelize.define('user', {
-        username: {
-            type: DataTypes.STRING(20),
-            allowNull: false,
-            unique: true
-        },
-        password: {
-            type: DataTypes.STRING(255),
-            allowNull: false
-        },
-        realName: {
-            type: DataTypes.STRING(20),
-            allowNull: false
-        },
-        phoneNumber: {
-            type: DataTypes.STRING(11),
-            allowNull: false,
-            unique: true
-        },
-        APIKey: {
-            type: DataTypes.STRING(32),
-            allowNull: false
-        },
-    }, {
-        timestamps: true
-    });
-};
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('./index');
+
+class User extends Model {}
+User.init({
+    username: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING(255),
+        allowNull: false
+    },
+    realName: {
+        type: DataTypes.STRING(20),
+        allowNull: false
+    },
+    phoneNumber: {
+        type: DataTypes.STRING(11),
+        allowNull: false,
+        unique: true
+    },
+    APIKey: {
+        type: DataTypes.STRING(32),
+        allowNull: false
+    },
+}, {
+  sequelize,
+  modelName: 'user',
+  timestamps: true
+});
+
+module.exports = User;
