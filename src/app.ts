fbdc3cc9ed767ef764e6e82c0833f388626883a9
MYEONGGWANG BANG <idsan.net@hotmail.com>
2020-05-05 21:20:42 +0900

    MQTT를 이용해 센서 값 수집

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,52 @@
-import "reflect-metadata";
-import { createConnection } from "typeorm";
 import express from "express";
-// import morgan from "morgan"; // logger
+import morgan from "morgan";
+import MQTT from "async-mqtt";
+import bodyParser from "body-parser";
+import { DateTime } from "luxon";
 import "dotenv/config";
 
 import * as homeController from "./controllers/home";
 
 const app = express();
+// TODO DB연동
+const client = MQTT.connect(process.env.MQTT_SERVER);
 
-createConnection()
-  .then(async connection => {
-    console.log("DB 연결 됨");
-  })
-  .catch(error => console.log("TypeORM connection error: ", error));
-
+// Express 설정
 app.set("port", process.env.PORT || 3000);
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// app.use(morgan("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+/**
+ * MQTT 하우스 관련 topic 구독
+ */
+const doStuff = async () => {
+  console.log("Starting");
+  try {
+    client.on("message", (topic, message) => {
+      let parsedMessage = JSON.parse(
+        Buffer.from(message.toJSON().data).toString("utf-8")
+      );
+
+      console.log(`topic: ${topic}, date: ${DateTime.local().toString()}`);
+      // TODO DB에 데이터 저장
+      console.log(parsedMessage);
+    });
+
+    await client.subscribe("house/#");
+
+    console.log("Done");
+  } catch (e) {
+    console.log(e.stack);
+    process.exit();
+  }
+};
+
+client.on("connect", doStuff);
+
+/**
+ * 주요 라우트
+ */
 app.get("/", homeController.index);
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,14 @@
 import errorHandler from "errorhandler";
-
 import app from "./app";
-import { ApolloServer } from "apollo-server-express";
-import { schema } from "./graphql/schema";
 
 /**
  * Error Handler. Provides full stack - remove for production
  */
-// app.use(errorHandler());
+app.use(errorHandler());
 
 /**
  * Start Express server.
  */
-
-const apollo = new ApolloServer({ schema });
-apollo.applyMiddleware({ app });
 const server = app.listen(app.get("port"), () => {
   console.log(
     "  App is running at http://localhost:%d in %s mode",
